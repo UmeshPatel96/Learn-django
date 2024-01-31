ebaa19cead03f5d7cb2630af1937b079815b3e4d
UmeshPatel96 <up962002@gmail.com>
2024-01-31 12:07:36 +0530

    commit

---
+++ day7/marks.js
@@ -0,0 +1,13 @@
+let marks =[
+    {
+        "name":umesh,
+        "marks":{
+            "English":50,
+            "Maths":50,
+            "science":50,
+        }
+    }
+]
+marks.map((m)=>{
+    console.log(m.name)
+})
